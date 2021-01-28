b4c0af8700ab2adc3dc31310b569e02df866eb33
Laura <mail@laura.fm>
2021-01-28 18:24:17 +0100

    Challenges 6 and 7

--- src/roam_seven_gui_challenge/core.cljs
+++ src/roam_seven_gui_challenge/core.cljs
@@ -213,8 +213,7 @@
   (js/setInterval 
     (fn []
       (if (< @deciseconds-elapsed @duration)
-        (swap! deciseconds-elapsed (fn [ms] (+ ms 1)))
-        (js/console.log "finished"))) 100))
+        (swap! deciseconds-elapsed (fn [ms] (+ ms 1))))) 100))
 
 (defn component-4 []
   [:div {:class "timer"}
@@ -248,14 +247,14 @@
     (generate-person-id) (create-person "Hans" "Emil")
     (generate-person-id) (create-person "Max" "Mustermann")
     (generate-person-id) (create-person "Roman" "Tisch"))
-  :selected-person nil
+  :selected-person-id nil
   :first-name ""
   :last-name ""}))
 
 (defn event-handler [state [event-name value]]
   (case event-name
     :change-selection 
-      (assoc state :selected-person value
+      (assoc state :selected-person-id (first value)
                    ; I'm sure there's an easier alternative for dealing 
                    ; with [k v] pairs from a hashmap
                    ; last feels like a hack
@@ -274,12 +273,15 @@
                (create-person (get state :first-name) (get state :last-name))}))
     :update-entry
       (assoc state :names
-        (assoc-in 
-          (assoc-in (get state :names) [(first (get state :selected-person)) :first-name] (get state :first-name))
-          [(first (get state :selected-person)) :last-name] (get state :last-name)))
+        (update-in
+          (get state :names)
+          [(get state :selected-person-id)]
+          assoc
+          :first-name (get state :first-name)
+          :last-name (get state :last-name)))
     :delete-entry
       (assoc state :names
-        (dissoc (get state :names) (first (get state :selected-person))))
+        (dissoc (get state :names) (get state :selected-person-id)))
     state))
 
 (defn crud-emit [e]
@@ -297,8 +299,7 @@
   (let [filtered-names 
     (filter (fn [[k v]] 
       (str/starts-with? (get v :last-name) filter-value)) names)]
-    (tlog filtered-names)
-    [:select {:size 5
+  [:select {:size 5
               :on-change #(crud-emit [:change-selection (nth filtered-names (-> % .-target .-selectedIndex))])}
       (for [[k name] filtered-names]
         (let [displayname (gstring/format "%s, %s" (get name :first-name) (get name :last-name))]
@@ -339,6 +340,318 @@
    [:h2 "CRUD"]
    [crud crud-state]])
 
+
+;; -------------------------
+;; Circle Drawer
+
+; need to highlight circles, so need to store all circle positions somewhere
+; need to keep a history of actions for redo and undo
+; I'm pretty sure I need a form-3 component to get the canvas ctx
+
+; my first time succesfully destructuring associative structures! yay
+(defn distance-between-points [{x1 :x y1 :y} {x2 :x y2 :y}]
+  (Math/sqrt 
+    (+ 
+      (Math/pow (- y1 y2) 2)
+      (Math/pow (- x1 x2) 2))))
+
+; still not as good as it can be, but at least this is supposed to 
+; be a little bit more reasonable compared to my earlier approach.
+(defn crypto-generate-id []
+  (let [v (js/Uint32Array. 1)]
+    (first (.getRandomValues js/crypto v))))
+
+(defn clamp [value min-value max-value]
+  "Makes sure that value is never lower than min and never higher than max"
+  (max min-value (min value max-value)))
+
+; this will work as follows:
+; we have a list of actions taken. This is recorded in :action-history
+; along with this history, we have a function that will compute the state of the circles; a projection
+; that projection is what's going to be rendered.
+
+;; actions
+; an action has a name and a list of values
+; (1) :action-add-circle {:x <num> :y <num> :id <num>}
+; (2) :action-resize-circle {:new-radius :id}
+(def circle-drawer-state (r/atom {
+  :action-history [
+    [:action-add-circle {:x 30 :y 40 :id 5}]
+    [:action-add-circle {:x 75 :y 100 :id 12}]
+    [:action-resize-circle {:id 5 :new-radius 25}]
+  ]
+  ; points to where history is cut-off (via undo)
+  :history-index 2
+  :highlight-id nil
+  :adjusting-diameter-id nil
+  :adjusting-diameter-values nil
+}))
+
+(defn project-circles [state]
+  (let [history (get state :action-history)
+        history-index (get state :history-index)
+        resizing-id (get state :adjusting-diameter-id)
+        resizing-r (get-in state [:adjusting-diameter-values :r])
+        circles 
+        (reduce 
+          (fn [circles [name value]]
+            (case name
+              :action-add-circle 
+                (assoc circles 
+                  (get value :id)
+                  {:x (get value :x)
+                    :y (get value :y)
+                    :r 10})
+              :action-resize-circle 
+                (assoc-in circles [(get value :id) :r] (get value :new-radius))))
+          {}
+          (take (+ 1 history-index) history))]
+        (if (not (nil? resizing-id))
+          (assoc-in circles [resizing-id :r] resizing-r)
+          circles)))
+
+(comment (if (= (get state :adjusting-diameter-id) (get value :id))
+                  (get-in state [:adjusting-diameter-values :r])
+                  10))
+
+(defn add-to-history [state action]
+  (assoc state
+    :action-history
+      (conj (vec (take (+ 1 (get state :history-index)) (get state :action-history))) action)
+    :history-index
+      (+ 1 (get state :history-index))))
+
+; as this event-handler grows bigger and bigger
+; the name "value" that can contain anything ranging from nil to a complex and nested associative array
+; is there a way to name the values that come out of value within the event-handlers themselves?
+(defn circle-event-handler [state [event-name value]]
+  (case event-name
+    :finalize-selected-circle-diameter
+      ; these are two distinct sequential operations in my mental-model
+      ; but they're written here as a nested operation because of how immutability operations work.
+      ; I believe you can make nested code -look- sequential with a macro? Something like (-> a b)?
+      (assoc (add-to-history state [:action-resize-circle 
+        {:id (get state :adjusting-diameter-id) :new-radius (get-in state [:adjusting-diameter-values :r])}])
+        :adjusting-diameter-id nil
+        :adjusting-diameter-values nil)
+    :adjust-selected-circle-diameter
+      (assoc-in state [:adjusting-diameter-values :r] value)
+    :open-adjust-parameter-popup
+      (assoc state 
+        :adjusting-diameter-id (get (tlog value) :id)
+        :adjusting-diameter-values (get value :circle))
+    :undo
+      (assoc state :history-index 
+        (clamp 
+          (- (get state :history-index) 1) 
+          -1
+          (- (count (get state :action-history)) 1)))
+    :redo
+      (assoc state :history-index 
+        (clamp
+          (+ (get state :history-index) 1)
+          -1
+          (- (count (get state :action-history)) 1)))
+    :draw-circle 
+      (add-to-history state [:action-add-circle {:x (get value :x) :y (get value :y) :id (crypto-generate-id)}])
+    :highlight 
+    (assoc state :highlight-id
+      (first 
+        (reduce-kv 
+          (fn [lowest k v]
+            (let [distance (distance-between-points v value)]
+              (if 
+                (< distance (last lowest))
+                [k distance]
+                lowest)))
+          [-1 (.-MAX_SAFE_INTEGER js/Number)]
+          (project-circles state))))
+    state))
+
+(defn circle-emit! [e]
+  (r/rswap! circle-drawer-state circle-event-handler e))
+
+(defn undo-redo-controls []
+  [:div {:class "undo-redo-controls"}
+   [:input {
+    :value "undo"
+    :type "button"
+    :on-click #(circle-emit! [:undo])}]
+   [:input {
+    :value "redo"
+    :type "button"
+    :on-click #(circle-emit! [:redo])}]])
+
+(defn draw-state [state ctx]
+  (dorun (for [[id {x :x y :y r :r}] (project-circles @state)]
+    (do
+      (.beginPath ctx)
+      (.arc ctx x y r 0 (* 2 Math/PI))
+      (if (= id (get @state :highlight-id))
+        (.fill ctx)
+        (.stroke ctx))))))
+
+(defn clear-ctx [ctx]
+  (let [canvas (. ctx -canvas)]
+    (.clearRect ctx 0 0 (. canvas -width) (. canvas -height))))
+
+(defn get-circle [state circle-id]
+  (get (project-circles @state) circle-id))
+
+(defn adjust-diameter-controls [state]
+  (fn [state]
+    (let [{x :x y :y r :r} (get @state :adjusting-diameter-values)]
+    [:div 
+     {:style
+      {:display (if (get @state :adjusting-diameter-id) "" "none")}}
+     [:p (gstring/format "Adjust diameter of circle at (%s, %s)." x y)]
+     [:input 
+      {:type "range"
+       :min 3
+       :max 25
+       :value (get (get @state :adjusting-diameter-values) :r)
+       :on-change #(circle-emit! [:adjust-selected-circle-diameter (-> % .-target .-value)])
+       :on-mouse-up #(circle-emit! [:finalize-selected-circle-diameter (-> % .-target .-value)])}]])))
+
+; see this github issue for dom-node deprecated warning.
+; use ref callback on reagent-render instead of (dom-node component)
+; https://github.com/reagent-project/reagent/issues/251#issuecomment-232186617
+(defn circle-canvas [state]
+  (let [canvas (atom nil)]
+    (r/create-class
+      {:component-did-mount
+        (fn [this]
+          (let [ctx (.getContext @canvas "2d")]
+            (clear-ctx ctx)
+            (draw-state state ctx)))
+       :component-did-update
+       (fn [this _]
+          (let [ctx (.getContext @canvas "2d")]
+            (clear-ctx ctx)
+            (draw-state state ctx)))
+       :reagent-render
+        (fn [state]
+          ; really stupid, but we -need- to dereference state
+          ; in the reagent-render function for component-did-update to get called
+          ; even though no rendering happens within this function >_>
+          (do @state)
+          [:canvas 
+            {
+              :on-mouse-up 
+              #(case (-> % .-nativeEvent .-button)
+                0 (circle-emit! [:draw-circle {:x (-> % .-nativeEvent .-offsetX) :y (-> % .-nativeEvent .-offsetY) :r 10}])
+                2 (circle-emit! [:open-adjust-parameter-popup 
+                  {:circle (get-circle state (get @state :highlight-id)) :id (get @state :highlight-id)}])
+                ())
+             :on-context-menu #(.preventDefault %)
+             :on-mouse-move #(circle-emit! [:highlight {:x (-> % .-nativeEvent .-offsetX) :y (-> % .-nativeEvent .-offsetY)}])
+             :ref (fn [ref] (reset! canvas ref))}])})))
+
+(defn circle-drawer [state]
+  (fn [state]
+    [:div
+     [undo-redo-controls]
+     [circle-canvas state]
+     [adjust-diameter-controls state]]))
+
+(defn component-6 []
+  [:div {:class "circle-drawer"}
+   [:h2 "Circle Drawer"]
+   [circle-drawer circle-drawer-state]])
+
+;; -------------------------
+;; Cells
+
+(defn make-text-parsable [text]
+  {:text text :cursor 0 :parsed ""})
+
+(defn read-one [{text :text cursor :cursor}]
+  (if (nil? cursor) 
+    false
+    (nth text cursor)))
+
+(defn advance [input]
+  (assoc input :cursor 
+    (inc (get input :cursor))))
+
+(defn success [input parsed]
+  (assoc input :parsed 
+    (str (get input :parsed) parsed)))
+
+(defn success? [input]
+  (not (false? input)))
+
+(defn lit [char]
+  (fn [input]
+    (let [r (read-one input)]
+      (if (= r char)
+        (success (advance input) char)
+        false))))
+
+(defn p-or [p1 p2]
+  (fn [input]
+    (let 
+      [r1 (p1 input)
+       r2 (p2 input)] 
+      (or
+        (when (success? r1) r1)
+        (when (success? r2) r2)
+        false))))
+
+(defn p-and [p1 p2]
+  (fn [input]
+    (let 
+      [r (p2 (p1 input))] 
+      (if (success? r) 
+        r
+        false))))
+
+(def a (lit "a"))
+(def b (lit "b")) 
+(def ab (p-or (lit "a") (lit "b")))
+(def abc (p-and (p-and (lit "a") (lit "b")) (lit "c")))
+
+(def t (make-text-parsable "apples"))
+
+(a t)
+
+(comment (defn a [c]
+  (fn [input] 
+    ((let [r readOne(input)])))))
+
+(comment
+  (defn reference [in] 
+    (re-matches #"[a-zA-Z]\w*" in))
+
+  (defn decimal [in] 
+    (#"-?\d+(\.\d*)?"))
+
+  (defn cell [in]
+    (let [result (re-matches #"[a-zA-Z]\d+")])
+      {:column (nth result 0)
+       :row (subs result 1)})
+
+  (defn range [in] (re-matches #"[a-zA-Z]"))
+
+  (defn tokenizer [string]
+    ())
+
+  (defn parser [string]
+    ()))
+
+
+(def cell-state (r/atom {
+
+}))
+
+(defn cells [state]
+  [:div "hi"])
+
+(defn component-7 []
+  [:div {:class "cells"}
+   [:h2 "Cells"]
+   [cells cell-state]])
+
 ;; -------------------------
 ;; Seven gui main
 
@@ -350,7 +663,9 @@
    [component-3]
    [component-4]
    [component-5]
-   ])
+   [component-6]
+   [component-7]
+])
   
 ;; -------------------------
 ;; Initialize app
